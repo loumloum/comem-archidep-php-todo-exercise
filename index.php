f17d339410cad9cbb62afd434f0840355472b0ee
loumloum <56160728+loumloum@users.noreply.github.com>
2020-01-10 14:31:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ define('DB_USER', getenv('TODOLIST_DB_USER') ?: 'todolist');
 define('DB_PASS', getenv('TODOLIST_DB_PASS'));
 define('DB_NAME', getenv('TODOLIST_DB_NAME') ?: 'todolist');
 define('DB_HOST', getenv('TODOLIST_DB_HOST') ?: '127.0.0.1');
-define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '8889');
+define('DB_PORT', getenv('TODOLIST_DB_PORT') ?: '3306');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
